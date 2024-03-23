99f21068e018e5b79a767ae5b69464be324546e9
DigiDude21 <104496357+DigiDude21@users.noreply.github.com>
2024-03-23 18:20:53 +0800

    Test Cases

--- js/main.js
+++ js/main.js
@@ -258,6 +258,12 @@ function convertToLowerCase(str) {
     });
 }
 
+function countDecimals(num) {
+    // this counts the decimals to subtract from the limit
+    if (Math.floor(num) === num || num === Infinity) return 0;
+    return num.toString().split(".")[1].length || 0; 
+}
+
 function specialCases() {
     console.log("Test Special");
 
@@ -297,377 +303,108 @@ function specialCases() {
                     mantissa = 20;
                     hex = 6;
                     exponent_limit = 90;
+                    neg_exponent_limit = -101;
                     break;
                 case "double":
                     exponent = 8;
                     mantissa = 50;
                     hex = 14;
                     exponent_limit = 369;
+                    neg_exponent_limit = -398;
                     break;
                 case "quadruple":
                     exponent = 12;
                     mantissa = 110;
                     hex = 30;
                     exponent_limit = 6111;
-                    break;
-            }
-            break;
-        case "binary":
-            switch (test_precision){
-                case "single":
-                    exponent = 8;
-                    mantissa = 27;
-                    hex = 8;
-                    exponent_limit = 127;
-                    break;
-                case "double":
-                    exponent = 11;
-                    mantissa = 52;
-                    hex = 16;
-                    exponent_limit = 1023;
-                    break;
-                case "quadruple":
-                    exponent = 15;
-                    mantissa = 112;
-                    hex = 32;
-                    exponent_limit = 16383;
+                    neg_exponent_limit = -6176;
                     break;
             }
             break;
     }
-
-    if (test_representation == "decimal"){
-        // if its NaN
-        if(test_input == "nan" || test_input == "qnan" || test_input == "snan"){
-            output_sign = "0";
-
-            output_combination = "1".repeat(5); 
-            
-            output_exponent = "0".repeat(exponent); 
-            output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
-
-            output_mantissa = "0".repeat(mantissa); 
-            output_mantissa = output_mantissa.match(/.{1,10}/g).join(" ");
-
-            output_hex = "7C" + "0".repeat(hex);
-            output_hex = output_hex.match(/.{1,4}/g).join(" ");
-
-            output_binary = output_sign + " " + output_combination + " " + output_exponent + " " + output_mantissa;
-            you_are_my_special = true;
+    
+    if (!isNaN(test_input) && isFinite(test_input)) {
+        if (/\d\.\d/.test(test_input.toString())) {
+            exponent_limit += countDecimals(test_input);
+           // console.log("Decimal count:", countDecimals(test_input));
         }
+    } 
+
+    // if its NaN
+    if(test_input == "nan" || test_input == "qnan" || test_input == "snan"){
+        output_sign = "0";
+
+        output_combination = "1".repeat(5); 
         
-        // if its +Infinity
-        else if(test_input == "inf" || (parseInt(test_exponent) > exponent_limit && test_input.charAt(0) != '-')){
-            output_sign = "0";
+        output_exponent = "0".repeat(exponent); 
+        output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
 
-            output_combination = "1".repeat(4) + "0"; 
-            
-            output_exponent = "0".repeat(exponent); 
-            output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
+        output_mantissa = "0".repeat(mantissa); 
+        output_mantissa = output_mantissa.match(/.{1,10}/g).join(" ");
 
-            output_mantissa = "0".repeat(mantissa); 
-            output_mantissa = output_mantissa.match(/.{1,10}/g).join(" ");
+        output_hex = "7C" + "0".repeat(hex);
+        output_hex = output_hex.match(/.{1,4}/g).join(" ");
 
-            output_hex = "78" + "0".repeat(hex);
-            output_hex = output_hex.match(/.{1,4}/g).join(" ");
-
-            output_binary = output_sign + " " + output_combination + " " + output_exponent + " " + output_mantissa;
-            you_are_my_special = true;
-        }
-        // if its -Infinity
-        else if(test_input == "-inf" || (parseInt(test_exponent) > exponent_limit && test_input.charAt(0) == '-')){
-            output_sign = "1";
-
-            output_combination = "1".repeat(4) + "0"; 
-            
-            output_exponent = "0".repeat(exponent); 
-            output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
-
-            output_mantissa = "0".repeat(mantissa); 
-            output_mantissa = output_mantissa.match(/.{1,10}/g).join(" ");
-
-            output_hex = "F8" + "0".repeat(hex);
-            output_hex = output_hex.match(/.{1,4}/g).join(" ");
-
-            output_binary = output_sign + " " + output_combination + " " + output_exponent + " " + output_mantissa;
-            you_are_my_special = true;
-        }
+        output_binary = output_sign + " " + output_combination + " " + output_exponent + " " + output_mantissa;
+        you_are_my_special = true;
     }
 
-    else if(test_representation == "binary"){
-        // If its +0 or -0
-        if (parseFloat(test_input) == 0) {
-    
-            if(test_sign){
-                output_sign = "0";
-                output_hex = "0".repeat(hex); 
-                output_hex = output_hex.match(/.{1,4}/g).join(" "); 
-            }
-
-            else{
-                output_sign = "1";
-                output_hex = "8" + "0".repeat(hex - 1); 
-                output_hex = output_hex.match(/.{1,4}/g).join(" "); 
-            }
-            
-            if(test_precision == "single"){
-                output_exponent = "0".repeat(exponent); 
-                output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
-
-                output_mantissa = "0".repeat(3) + " ";
-
-                for (let i = 0; i < 20; i += 4) {
-                    output_mantissa += "0".repeat(4) + " "; 
-                } 
-
-                output_mantissa = output_mantissa.trim();
-            }
-
-            else{
-                output_exponent = "0".repeat(exponent - (exponent - 3)) + " ";
-
-                for (let i = 0; i < (exponent - 3); i += 4) {
-                    output_exponent += "0".repeat(4) + " "; 
-                } 
-
-                output_exponent = output_exponent.trim();
-
-                output_mantissa = "0".repeat(mantissa); 
-                output_mantissa = output_mantissa.match(/.{1,4}/g).join(" ");
-            }
-
-            output_binary = output_sign + " " + output_exponent + " " + output_mantissa;
-            you_are_my_special = true;
-        }
-
-        // If its Denormalized
-        if (test_input == "dnrm") {
-    
-            output_sign = "0";
-
-            if(test_precision == "single"){
-                output_exponent = "0".repeat(exponent); 
-                output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
-
-                output_mantissa = "1" + "0".repeat(2) + " ";
-
-                for (let i = 0; i < (exponent - 3); i += 4) {
-                    output_mantissa += "0".repeat(4) + " "; 
-                } 
-
-                output_mantissa = output_mantissa.trim();
-
-                output_hex = "0".repeat(2) + "4" + "0".repeat(hex - 3); 
-            }
-
-            else{
-                output_exponent = "0".repeat(3) + " ";
-
-                for (let i = 0; i < (exponent - 3); i += 4) {
-                    output_exponent += "0".repeat(4) + " "; 
-                } 
-
-                output_exponent = output_exponent.trim();
-
-                output_mantissa = "1" + "0".repeat(mantissa - 1); 
-                output_mantissa = output_mantissa.match(/.{1,4}/g).join(" ");
-
-                if (test_precision == "double"){
-                    output_hex = "0".repeat(3) + "8" + "0".repeat(hex - 4); 
-                }
-
-                else{
-                    output_hex = "0".repeat(4) + "8" + "0".repeat(hex - 5); 
-                }
-            }
-
-            output_hex = output_hex.match(/.{1,4}/g).join(" "); 
-
-            output_binary = output_sign + " " + output_exponent + " " + output_mantissa;
-            you_are_my_special = true;
-        }
-
-        // If its sNaN
-        else if (test_input == "snan") {
-            output_sign = "0";
-
-            if(test_precision == "single"){
-                output_exponent = "1".repeat(exponent); 
-                output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
-
-                output_mantissa = "01" + "0".repeat(1) + " ";
-
-                for (let i = 0; i < 20; i += 4) {
-                    output_mantissa += "0".repeat(4) + " "; 
-                } 
-
-                output_mantissa = output_mantissa.trim();
-                output_hex = "7" + "F".repeat(1) + "4" + "0".repeat(hex - 3);
-            }
-
-            else{
-                output_exponent = "1".repeat(3) + " ";
-
-                for (let i = 0; i < (exponent - 3); i += 4) {
-                    output_exponent += "1".repeat(4) + " "; 
-                } 
-
-                output_exponent = output_exponent.trim();
-
-                output_mantissa = "01" + "0".repeat(mantissa - 2); 
-                output_mantissa = output_mantissa.match(/.{1,4}/g).join(" ");
-
-                if (test_precision == "double"){
-                    output_hex = "7" + "F".repeat(2) + "4" + "0".repeat(hex - 4);
-                }
-
-                else{
-                    output_hex = "7" + "F".repeat(3) + "4" + "0".repeat(hex - 5);
-                }
-            }
-
-            output_hex = output_hex.match(/.{1,4}/g).join(" "); 
-             
-            output_binary = output_sign + " " + output_exponent + " " + output_mantissa;
-            you_are_my_special = true;
-        }
-
-        // If its qNaN
-        else if (test_input == "qnan") {
-            output_sign = "0";
-
-            if(test_precision == "single"){
-                output_exponent = "1".repeat(exponent); 
-                output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
-
-                output_mantissa = "1" + "0".repeat(2) + " ";
-
-                for (let i = 0; i < 20; i += 4) {
-                    output_mantissa += "0".repeat(4) + " "; 
-                } 
-
-                output_mantissa = output_mantissa.trim();
-                output_hex = "7" + "F".repeat(1) + "8" + "0".repeat(hex - 3);
-            }
-
-            else{
-                output_exponent = "1".repeat(3) + " ";
-
-                for (let i = 0; i < (exponent - 3); i += 4) {
-                    output_exponent += "1".repeat(4) + " "; 
-                } 
-
-                output_exponent = output_exponent.trim();
-
-                output_mantissa = "1" + "0".repeat(mantissa - 1); 
-                output_mantissa = output_mantissa.match(/.{1,4}/g).join(" ");
-
-                if (test_precision == "double"){
-                    output_hex = "7" + "F".repeat(2) + "8" + "0".repeat(hex - 4);
-                }
-
-                else{
-                    output_hex = "7" + "F".repeat(3) + "8" + "0".repeat(hex - 5);
-                }
-            }
-
-            output_hex = output_hex.match(/.{1,4}/g).join(" "); 
-             
-            output_binary = output_sign + " " + output_exponent + " " + output_mantissa;
-            you_are_my_special = true;
-        }
-
-        // If its +Infinity
-        else if (test_input == "inf" || (parseInt(test_exponent) > exponent_limit && test_input.charAt(0) != '-')) {
-            output_sign = "0";
-
-            if(test_precision == "single"){
-                output_exponent = "1".repeat(exponent); 
-                output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
-
-                output_mantissa = "0".repeat(3) + " ";
-
-                for (let i = 0; i < 20; i += 4) {
-                    output_mantissa += "0".repeat(4) + " "; 
-                } 
-
-                output_mantissa = output_mantissa.trim();
-                output_hex = "7" + "F".repeat(1) + "8" + "0".repeat(hex - 3);
-            }
-
-            else{
-                output_exponent = "1".repeat(3) + " ";
-
-                for (let i = 0; i < (exponent - 3); i += 4) {
-                    output_exponent += "1".repeat(4) + " "; 
-                } 
-
-                output_exponent = output_exponent.trim();
-
-                output_mantissa = "0".repeat(mantissa); 
-                output_mantissa = output_mantissa.match(/.{1,4}/g).join(" ");
-
-                if (test_precision == "double"){
-                    output_hex = "7" + "F".repeat(2) + "0".repeat(hex - 3);
-                }
-
-                else{
-                    output_hex = "7" + "F".repeat(3) + "0".repeat(hex - 4);
-                }
-            }
-
-            output_hex = output_hex.match(/.{1,4}/g).join(" "); 
-             
-            output_binary = output_sign + " " + output_exponent + " " + output_mantissa;
-            you_are_my_special = true;
-        }
-
-        // If its -Infinity
-        else if (test_input == "-inf" || (parseInt(test_exponent) > exponent_limit && test_input.charAt(0) == '-')) {
-            output_sign = "1";
-
-            if(test_precision == "single"){
-                output_exponent = "1".repeat(exponent); 
-                output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
-
-                output_mantissa = "0".repeat(3) + " ";
-
-                for (let i = 0; i < 20; i += 4) {
-                    output_mantissa += "0".repeat(4) + " "; 
-                } 
-
-                output_mantissa = output_mantissa.trim();
-                output_hex = "F".repeat(2) + "8" + "0".repeat(hex - 3);
-            }
-
-            else{
-                output_exponent = "1".repeat(3) + " ";
-
-                for (let i = 0; i < (exponent - 3); i += 4) {
-                    output_exponent += "1".repeat(4) + " "; 
-                } 
-
-                output_exponent = output_exponent.trim();
-
-                output_mantissa = "0".repeat(mantissa); 
-                output_mantissa = output_mantissa.match(/.{1,4}/g).join(" ");
-
-                if (test_precision == "double"){
-                    output_hex = "F".repeat(3) + "0".repeat(hex - 3);
-                }
-
-                else{
-                    output_hex = "F".repeat(4) + "0".repeat(hex - 4);
-                }
-            }
-
-            output_hex = output_hex.match(/.{1,4}/g).join(" "); 
-             
-            output_binary = output_sign + " " + output_exponent + " " + output_mantissa;
-            you_are_my_special = true;
-        }
+    // if the exponent is below the limit
+    else if(parseInt(test_exponent) < neg_exponent_limit){
+        output_sign = "0";
+
+        output_combination = "0".repeat(5); 
+        
+        output_exponent = "0".repeat(exponent); 
+        output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
+
+        output_mantissa = "0".repeat(mantissa); 
+        output_mantissa = output_mantissa.match(/.{1,10}/g).join(" ");
+
+        output_hex = "0".repeat(hex + 2);
+        output_hex = output_hex.match(/.{1,4}/g).join(" ");
+
+        output_binary = output_sign + " " + output_combination + " " + output_exponent + " " + output_mantissa;
+        you_are_my_special = true;
     }
+    
+    // if its +Infinity
+    else if ((test_input == "inf" || ((test_input.replace(/\./g, '').length + parseInt(test_exponent)) > exponent_limit + 1)) && test_input.charAt(0) != '-') {
+        output_sign = "0";
 
+        output_combination = "1".repeat(4) + "0"; 
+        
+        output_exponent = "0".repeat(exponent); 
+        output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
+
+        output_mantissa = "0".repeat(mantissa); 
+        output_mantissa = output_mantissa.match(/.{1,10}/g).join(" ");
+
+        output_hex = "78" + "0".repeat(hex);
+        output_hex = output_hex.match(/.{1,4}/g).join(" ");
+
+        output_binary = output_sign + " " + output_combination + " " + output_exponent + " " + output_mantissa;
+        you_are_my_special = true;
+    }
+    // if its -Infinity
+    else if(test_input == "-inf" || (test_input.replace(/[.-]/g, '').length + parseInt(test_exponent) > exponent_limit + 1 && test_input.charAt(0) == '-')){
+        output_sign = "1";
+
+        output_combination = "1".repeat(4) + "0"; 
+        
+        output_exponent = "0".repeat(exponent); 
+        output_exponent = output_exponent.match(/.{1,4}/g).join(" ");
+
+        output_mantissa = "0".repeat(mantissa); 
+        output_mantissa = output_mantissa.match(/.{1,10}/g).join(" ");
+
+        output_hex = "F8" + "0".repeat(hex);
+        output_hex = output_hex.match(/.{1,4}/g).join(" ");
+
+        output_binary = output_sign + " " + output_combination + " " + output_exponent + " " + output_mantissa;
+        you_are_my_special = true;
+    }
+    
 }
 
 
@@ -680,11 +417,11 @@ input_form.addEventListener("submit", function (event) {
 
     // Test inputs for special cases
     you_are_my_special = false
-    specialCases()
-    // if (input_representation == "decimal") {
-    //     specialCases()
-    // }
+    if (input_representation == "decimal") {
+        specialCases()
+    }
     if (you_are_my_special) {
+        input_error_element.style.display = "none";
         putOutputs();
         return;
     }
